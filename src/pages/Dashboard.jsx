a4fae01a5a22181cb88447b740099fade5fcace1
randikasptra <saputrarandika857@gmail.com>
2025-03-16 11:50:50 +0700

    Menata struktur folder

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <p>Selamat datang di halaman Dashboard!</p>
+        </div>
+    );
+};
+
+export default Dashboard; // Pastikan ada export default
---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Dashboard from "../pages/Dashboard";
+import Reservation from "../pages/Reservation";
+import History from "../pages/History";
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/reservation" element={<Reservation />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
