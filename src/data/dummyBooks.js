505877bc95e1d155a65259dc9abbd9d57c225170
randikasptra <saputrarandika857@gmail.com>
2025-05-25 18:34:09 +0700

    add/dummyUsers

--- src/data/dummyBooks.js
+++ src/data/dummyBooks.js
@@ -2,26 +2,30 @@
 
 const dummyBooks = [
     {
-        id_buku: "B001",
-        judul_buku: "Belajar React",
-        pengarang: "Dewi",
-        penerbit: "Graha Ilmu",
-        tahun_terbit: "2022",
-        jumlah_stok: 5,
-        status: "Tersedia",
-        id_kategory: "KAT01",
-    },
-    {
-        id_buku: "B002",
-        judul_buku: "Algoritma Dasar",
-        pengarang: "Joko",
-        penerbit: "Erlangga",
+        id_buku: "BK001",
+        judul_buku: "Pemrograman JavaScript",
+        pengarang: "Budi Hartono",
+        penerbit: "Gramedia",
         tahun_terbit: "2020",
-        jumlah_stok: 3,
-        status: "Dipinjam",
-        id_kategory: "KAT02",
+        jumlah_stok: 12,
+        status: "Tersedia",
+        id_kategory: "KAT001",
+        cover: "/images/js-book.jpg", // atau link gambar
     },
-    // Tambah lagi kalau perlu
+
+    {
+        id_buku: "BK002",
+        judul_buku: "Pemrograman Python",
+        pengarang: "Budi Hartono",
+        penerbit: "Gramedia",
+        tahun_terbit: "2020",
+        jumlah_stok: 12,
+        status: "Tersedia",
+        id_kategory: "KAT001",
+        cover: "/images/js-book.jpg",
+    }
+
+   
 ];
 
 export default dummyBooks;
